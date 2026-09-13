5868d5b950a2ea11bc5bd5ced63566318bd0d6f8
antmyak-ipt28 <antmyak-ipt28@lll.kpi.ua>
2026-09-13 21:37:47 +0300

    Added functions for calculating probability distributions

--- lab1/ФБ42_M'якенький_Кисельова/lab1.py
+++ lab1/ФБ42_M'якенький_Кисельова/lab1.py
@@ -9,9 +9,39 @@ def preprocess(filename):
     text_nospaces = re.sub(r' ', '', text)
     return (text, text_nospaces)
 
+def prob_dist_1(text):
+    dist = {}
+    for symbol in text:
+        dist[symbol] = dist.get(symbol, 0) + 1
+    for k in dist.keys():
+        dist[k] /= len(text)
+    return dist
+
+def prob_dist_2(text):
+    dist = {}
+    for i in range(0, len(text)-1):
+        bigram = text[i:i+2]
+        dist[bigram] = dist.get(bigram, 0) + 1
+    for k in dist.keys():
+        dist[k] /= len(text) - 1
+    return dist
+
+def prob_dist_2_no_intersect(text):
+    dist = {}
+    for i in range(0, len(text)-1, 2):
+        bigram = text[i:i+2]
+        dist[bigram] = dist.get(bigram, 0) + 1
+    for k in dist.keys():
+        dist[k] /= len(text) - 1
+    return dist
+
+
 def main():
-    # text, text_nospaces = preprocess("TEXT_UTF8")
-    pass
+    text, text_nospaces = preprocess("TEXT_UTF8")
+    dist = prob_dist_1(text)
+    print(dist)
+    dist = prob_dist_2(text)
+    print(dist)
 
 if __name__ == "__main__":
     main()
